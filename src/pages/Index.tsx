e4dee0348d2bab69d134e26335c4a748a1ff1db2
DesarrolloAlmango <desarrollo@almango.com.uy>
2025-05-06 08:47:51 -0300

    Prueba

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,7 +3,7 @@ import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '@/context/AuthContext';
 import LoginForm from '@/components/auth/LoginForm';
-
+//
 const Index = () => {
   const { currentUser, loading } = useAuth();
   const navigate = useNavigate();
